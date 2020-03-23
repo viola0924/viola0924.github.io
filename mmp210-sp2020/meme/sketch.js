5d73ba445ff57fc413afadfad72bfcf10f0abf05
viola0924 <60355637+viola0924@users.noreply.github.com>
2020-03-23 11:44:09 -0400

    Add files via upload

---
+++ mmp210-sp2020/meme/sketch.js
@@ -0,0 +1,23 @@
+let i=0.1
+function setup() {
+  frameRate(10);
+  createCanvas(400, 400);
+  img=loadImage("trump-2020.png");
+  img1=loadImage("2.jfif");
+}
+
+function draw() {
+  background(255);
+    textSize(25); 
+    image(img1,0,0,400,400);
+  fill(255,0,0);
+    text("Make America Great Again", mouseX, mouseY);
+   if (mouseIsPressed) 
+   {scale(i); i+=0.1
+  image(img,0,0,img.width/3,img.height/3);
+    text("2020!Surprise!",25,50);
+  if (i>2.5){ 
+        i=0.1
+    }
+  }
+}
